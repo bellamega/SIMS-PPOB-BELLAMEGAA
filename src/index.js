7888738c8fe9fc65fd17668aa0cbb0abc6cbff4f
bellamega <bella.mega49@gmail.com>
2025-01-29 18:27:06 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import './index.css';
 import App from './App';
-import store from './redux/Store'; // Pastikan path yang benar
+import store from './redux/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
+ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
+  document.getElementById('root')
 );
