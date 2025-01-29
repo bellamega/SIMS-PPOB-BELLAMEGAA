ab6377453ed2d7a735d9083f89599134724bb69e
bellamega <bella.mega49@gmail.com>
2025-01-29 19:53:10 +0700

    Update Store.js

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,13 +1,10 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
-import topUpReducer from './reducers'; // Ensure this is the correct path to your reducer
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './authSlice';
 
-// Combine your reducers here
-const rootReducer = combineReducers({
-  topUp: topUpReducer,
-  // You can add more reducers here if you have more
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
 });
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
