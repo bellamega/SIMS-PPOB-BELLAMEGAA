328b7d76781cfff61c7d49b58331197a97b5ba05
bellamega <bella.mega49@gmail.com>
2025-01-29 19:24:00 +0700

    Update actions.js

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -22,3 +22,23 @@ export const setError = (error) => ({
   type: 'SET_ERROR',
   payload: error,
 });
+
+export const setTopUpAmount = (amount) => ({
+  type: 'SET_TOP_UP_AMOUNT',
+  payload: amount,
+});
+
+export const setBalance = (balance) => ({
+  type: 'SET_BALANCE',
+  payload: balance,
+});
+
+export const setError = (error) => ({
+  type: 'SET_ERROR',
+  payload: error,
+});
+
+export const setSuccessMessage = (message) => ({
+  type: 'SET_SUCCESS_MESSAGE',
+  payload: message,
+});
