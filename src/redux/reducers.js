483fadde9308005300545cc9abee229852e99185
bellamega <bella.mega49@gmail.com>
2025-01-29 18:25:13 +0700

    Update reducers.js

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,9 +1,26 @@
-// reducers.js
-import { combineReducers } from '@reduxjs/toolkit';
-import registrationReducer from './registrationReducer'; // Update path ini sesuai lokasi file
+const initialState = {
+  profile: null,
+  balance: null,
+  services: [],
+  banner: null,
+  error: null,
+};
 
-const rootReducer = combineReducers({
-  registration: registrationReducer, // Tambahkan registrationReducer di sini
-});
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'SET_PROFILE':
+      return { ...state, profile: action.payload };
+    case 'SET_BALANCE':
+      return { ...state, balance: action.payload };
+    case 'SET_SERVICES':
+      return { ...state, services: action.payload };
+    case 'SET_BANNER':
+      return { ...state, banner: action.payload };
+    case 'SET_ERROR':
+      return { ...state, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 export default rootReducer;
