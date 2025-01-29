98c5b2053c7390200dbdab2cbcbe2de14790b701
bellamega <bella.mega49@gmail.com>
2025-01-29 19:17:14 +0700

    Update Store.js

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,6 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import topUpReducer from './reducers';
+
+// Gabungkan reducer jika ada lebih dari satu
+const rootReducer = combineReducers({
+  topUp: topUpReducer,
+});
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
